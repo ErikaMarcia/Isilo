c324ff91d2a5421f5bd3531ddce44c2905394e43
Arthur Bicalho Lana Corrêa Fernandes <1235804@sga.pucminas.br>
2022-04-04 22:18:28 -0300

    'Map'

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
