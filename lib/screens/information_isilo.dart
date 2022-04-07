faba1e70d6f65d0f62840a8a26127b8a65812f24
Arthur Bicalho Lana Corrêa Fernandes <1235804@sga.pucminas.br>
2022-04-07 18:45:11 -0300

    'Information'

---
+++ lib/screens/information_isilo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InformationIsilo extends StatefulWidget {
+  const InformationIsilo({Key? key}) : super(key: key);
+
+  @override
+  State<InformationIsilo> createState() => _InformationIsiloState();
+}
+
+class _InformationIsiloState extends State<InformationIsilo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
