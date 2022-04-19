d6417efa559560759002f570347d7a06f968c56f
Arthur Bicalho Lana Corrêa Fernandes <1235804@sga.pucminas.br>
2022-04-18 21:03:35 -0300

    Informações iSilo

--- lib/screens/information_isilo.dart
+++ lib/screens/information_isilo.dart
@@ -10,18 +10,70 @@ class InformationIsilo extends StatefulWidget {
 class _InformationIsiloState extends State<InformationIsilo> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        'Presta assistência a idosos de 60 a 90 anos que se encontre em situação de risco e/ou vulnerabilidade social.',
-        overflow: TextOverflow.visible,
-        textAlign: TextAlign.left,
-        style: TextStyle(
-          height: 1.6666666666666667,
-          fontSize: 15.0,
-          fontFamily: 'Nunito',
-          fontWeight: FontWeight.w600,
-          color: Color.fromARGB(255, 91, 132, 153),
-          /* letterSpacing: 0.0, */
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('iSilo'),
+        ),
+        body: Column(
+            children: const [
+              Center(
+                heightFactor: 2,
+                child:
+                Text(
+                    'Nome: iSilo BH',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 32,
+                      color: Colors.black,
+                    )
+                ),
+              ),
+              Center(
+                heightFactor: 2,
+                child: Text(
+                    'Sobre: iSilo para pessoas maiores de 60 anos',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 32,
+                      color: Colors.black,
+                    )
+                ),
+              ),
+              Center(
+                heightFactor: 2,
+                child: Text(
+                    'WhatsApp: (31) 99999-9999',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 32,
+                      color: Colors.black,
+                    )
+                ),
+              ),
+              Center(
+                heightFactor: 2,
+                child: Text(
+                    'Instruções: O iSilo não aceita animais e crianças de até 10 anos de idade.',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 32,
+                      color: Colors.black,
+                    )
+                ),
+              ),
+              Center(
+                heightFactor: 2,
+                child: Text(
+                    'Horário das visitas: Visita das 8h até as 17h.',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 32,
+                      color: Colors.black,
+                    )
+                ),
+              ),
+            ]
         ),
       ),
     );
