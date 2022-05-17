0bb106e6072fd9c0ea91c653dd76cffd24bbdf67
padraorezende <padraonxgalo@gmail.com>
2022-05-16 21:33:52 -0300

    SharedPreferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:isilo/db/database.dart';
 import 'package:isilo/screens/welcome.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setDouble("latitude", -19.9381372);
+  await prefs.setDouble("longitude", -43.9343437);
+  print('latitude ${prefs.getDouble("latitude")}');
+  print('longitude ${prefs.getDouble("longitude")}');
   runApp(
     MyApp(
       db: await $FloorAsylumDatabase.databaseBuilder('app_database.db').build(),
